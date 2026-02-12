3588fdd2b4484eb667a38a98aa6d99e14ead23ec
Gmmolly05 <gmm9098@rit.edu>
2026-02-11 19:30:12 -0500

    Implemented starter pinball mechanics
    - added ball; left; right paddle prefabs
    - added paddle script
    - added two physics material (ball/paddle)

---
+++ Assets/Scripts/PlayerPaddle.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+
+public class PlayerPaddle : MonoBehaviour
+{
+
+    [SerializeField]
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private bool invert;
+
+    private bool isMoving;
+    private float dir; 
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        dir = invert ? -1f : 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isMoving = Mouse.current.leftButton.isPressed;
+    }
+
+    void FixedUpdate()
+    {
+        if (isMoving)
+        {
+            rb.AddTorque(-speed * dir);
+        }
+        else
+        {
+            // this allows the paddle to return to beginning rotation
+            rb.AddTorque(speed * dir);
+        }
+    }
+}
