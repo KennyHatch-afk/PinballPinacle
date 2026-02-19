bbb458f4c8e9923427f0c41618fa9e3bf8d0e7c1
KennyHatch-afk <kah8090@g.rit.edu>
2026-02-19 18:48:38 -0500

    Added game over screen

--- Assets/Scripts/Stats.cs
+++ Assets/Scripts/Stats.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Stats : MonoBehaviour
 {
@@ -15,8 +16,10 @@ public class Stats : MonoBehaviour
     public GameObject gameCamera;
     public List<GameObject> levelPrefabs;
     public List<GameObject> levels;
-    public GameObject ScoreCounter;
-    public GameObject LifeCounter;
+    public GameObject scoreCounter;
+    public GameObject lifeCounter;
+    public GameObject gameOverText;
+    public GameObject gameOverScore;
     public float invincibilityTime;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -35,8 +38,34 @@ public class Stats : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ScoreCounter.GetComponent<TextMeshProUGUI>().text = "Score: " + Mathf.FloorToInt(score);
-        LifeCounter.GetComponent<TextMeshProUGUI>().text = "Lives: " + lives;
+        if(lives <= 0)
+        {
+            if(Mouse.current.leftButton.wasPressedThisFrame)
+            //Restart game when mouse is pressed
+            {
+                ball.SetActive(true);
+                scoreCounter.SetActive(true);
+                lifeCounter.SetActive(true);
+                gameOverText.SetActive(false);
+                gameOverScore.SetActive(false);
+                score = 0;
+                lives = 3;
+                ball.transform.SetLocalPositionAndRotation(new Vector3(-3, 5, 0), ball.transform.rotation);
+                maxHeight = ball.transform.position.y;
+                previousMaxHeight = maxHeight;
+
+                foreach (GameObject level in levels)
+                {
+                    Destroy(level);
+                }
+
+                int rand = Random.Range(1, levelPrefabs.Count);
+                levels.Add(Instantiate(levelPrefabs[rand], new Vector3(0, ball.transform.position.y + 12, ball.transform.position.z), Quaternion.identity));
+            }
+        }
+
+        scoreCounter.GetComponent<TextMeshProUGUI>().text = "Score: " + Mathf.FloorToInt(score);
+        lifeCounter.GetComponent<TextMeshProUGUI>().text = "Lives: " + lives;
 
         //Decrease invincibility timer and set the ball back to active if done
         if (invincibilityTime > 0)
@@ -57,15 +86,30 @@ public class Stats : MonoBehaviour
                 lives--;
             }
 
-            ball.transform.SetLocalPositionAndRotation(new Vector3(ball.transform.position.x, maxHeight, ball.transform.position.z), ball.transform.rotation);
+            //Game Over
+            if (lives <= 0)
+            {
+                ball.SetActive(false);
+                scoreCounter.SetActive(false);
+                lifeCounter.SetActive(false);
+                gameOverText.SetActive(true);
+                gameOverScore.SetActive(true);
+                gameOverScore.GetComponent<TextMeshProUGUI>().text = "Score: " + Mathf.FloorToInt(score);
+            }
+            else
+            //Respawn
+            {
+                ball.transform.SetLocalPositionAndRotation(new Vector3(ball.transform.position.x, maxHeight, ball.transform.position.z), ball.transform.rotation);
 
-            ball.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
-            invincibilityTime = 3;
+                ball.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+                invincibilityTime = 1;
+            }
         }
 
         //Set the score and max height if less than the ball's current position
         if (maxHeight < ball.transform.position.y)
         {
+            Debug.Log(score);
             score += ball.transform.position.y - maxHeight;
 
             maxHeight = ball.transform.position.y;
